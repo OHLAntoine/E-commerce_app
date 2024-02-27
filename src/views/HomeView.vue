f7b39c04069364c9b977eb9b2d42921dd995a839
OHLAntoine <antoine.ohl@gmail.com>
2024-02-27 19:06:50 +0100

    Mise en place du composant Card et home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <template>
 	<main>
-		<div class="container mx-auto px-4">
-			<h1 class="text-3xl font-bold">
-				Hello world!
-			</h1>
+		<div class="container mx-auto px-6 h-full">
+			<div class="pt-4">
+				<h1 class="text-center text-2xl font-bold">All Products</h1>
+			</div>
+			<!-- Product List -->
+			<section class="pb-5">
+				<div class="mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+					<div v-for="(i=0, index) in 8" :key="index">
+						<Card/>
+					</div>
+				</div>
+			</section>
 		</div>
 	</main>
 </template>
 
 <script setup>
-
+	import Card from '../components/items/Card.vue'
 </script>
